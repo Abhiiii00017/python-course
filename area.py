ef85d00bd1b2cf6538473ac1932dcb8594705c5f
Abhishek Kumar Sahu <abhiiii00177@gmail.com>
2023-02-06 12:32:11 +0530

    condition

---
+++ area.py
@@ -0,0 +1,6 @@
+#input ('promt')
+r = float(input ('Enter radius of circle'))
+area = 22/7*r**2
+print(f'Radius of circle is{r}')
+print(f'Radius of circle is{area}')
+
---
+++ condition.py
@@ -0,0 +1,6 @@
+x=input('please enter a number:')
+if x.isnumeric():
+    print('very good')
+else:
+    print('you stupid')
+
