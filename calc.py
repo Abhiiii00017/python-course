1ab73a7c45ab779d18dfb8d62e4470b8ede20db7
Abhishek Kumar Sahu <abhiiii00177@gmail.com>
2023-02-13 12:26:49 +0530

    loops

---
+++ calc.py
@@ -0,0 +1,33 @@
+import os 
+
+while True:
+    print('select a number')
+    print('1. additions')
+    print('2.Subtraction')
+    print('3.multiplication')
+    print('4. division')
+    print('5. exit')
+    ch= input('enter your choice:')
+    if ch=='1':
+        a=int(input('enter first number:'))
+        b=int(input('enter second number:'))
+        print(f'{a}+{b} is {a+b}')
+    elif ch == '2':
+        a=int(input('enter first number:'))
+        b=int(input('enter second number:'))
+        print(f'{a}-{b} is {a-b}')
+    elif ch == '3':
+        a=int(input('enter first number:'))
+        b=int(input('enter second number:'))
+        print(f'{a}*{b} is {a*b}')
+    elif ch == '4':
+        a=int(input('enter first number:'))
+        b=int(input('enter second number:'))
+        print(f'{a}/{b} is {a/b}')
+        print('exiting...')
+        break
+    else:
+        print('invalid choice')
+    input('press enter to continue..')
+    os.system('cls')
+    
